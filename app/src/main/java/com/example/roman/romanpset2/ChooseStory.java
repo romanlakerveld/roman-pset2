be0a4f1d2e032468d63dbe416d6d9c0cc7e7d478
romanlakerveld <31651999+romanlakerveld@users.noreply.github.com>
2017-11-09 15:22:44 +0100

    Nieuwe activity gemaakt commit

--- app/src/main/java/com/example/roman/romanpset2/ChooseStory.java
+++ app/src/main/java/com/example/roman/romanpset2/ChooseStory.java
@@ -2,15 +2,12 @@ package com.example.roman.romanpset2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.example.roman.romanpset2.Story
 
-public class MainActivity extends AppCompatActivity {
+public class ChooseStory extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_choose_story);
     }
-
-
 }
