a27fa7a1ccd460c1bef0d811e2ebc310036ad208
romanlakerveld <31651999+romanlakerveld@users.noreply.github.com>
2017-11-09 15:59:42 +0100

    Button met actie gemaakt

--- app/src/main/java/com/example/roman/romanpset2/MainActivity.java
+++ app/src/main/java/com/example/roman/romanpset2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.roman.romanpset2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.example.roman.romanpset2.Story
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.roman.romanpset2.Story;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void goToChoice(View view) {
+        TextView text = findViewById(R.id.textView);
+        text.setText("Rusland ist dat schones land");
+    }
 
 }
